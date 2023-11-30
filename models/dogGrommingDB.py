5585b299e0e7539523a3c39beda2cddeaeb0a059
Moe Ko <mko23@wooster.edu>
2023-11-29 23:38:44 -0500

    added comments in services_view and dogGroomingDB

--- models/dogGrommingDB.py
+++ models/dogGrommingDB.py
@@ -3,6 +3,7 @@ class DogGrommingDB:
         self._db_conn = db_conn
         self._cursor = db_cursor
     
+    # Retrieve all the dog entries from the dogs table
     def select_all_dogs(self):
         select_all_query = """
             SELECT * FROM dogs;
@@ -10,21 +11,25 @@ class DogGrommingDB:
         self._cursor.execute(select_all_query)
         return self._cursor.fetchall()
 
+    # Select all data from the custom view: service_dog_customer
     def select_all_services(self):
         select_all_query = """
             SELECT * FROM service_dog_customer;
         """
         self._cursor.execute(select_all_query)
 
-        # select_all_query = """
-        #     SELECT services.id, type, cost, completed, name, breed, age, CONCAT(firstname, ' ', lastname) AS full_name  FROM services
-        #     INNER JOIN dogs
-        #     ON services.dog_id = dogs.id
-        #     INNER JOIN customers
-        #     ON dogs.customer_id = customers.id;
-        # """
+        # This query below creates the custom view: service_dog_customer
+        # CREATE VIEW Service_Dog_Customer
+        # (id, type, cost, completed, name, breed, age, fullName)
+        # AS
+        # SELECT services.id, type, cost, completed, name, breed, age, CONCAT(firstname, ' ', lastname) AS full_name  FROM services
+        # INNER JOIN dogs
+        # ON services.dog_id = dogs.id
+        # INNER JOIN customers
+        # ON dogs.customer_id = customers.id;
         return self._cursor.fetchall()
     
+    # Retrieve only completed services
     def select_all_completed_services(self):
         select_all_services_query = """
             SELECT services.id, type, cost, completed, name, breed, age, CONCAT(firstname, ' ', lastname) AS full_name  FROM services
@@ -38,6 +43,7 @@ class DogGrommingDB:
 
         return self._cursor.fetchall()
     
+    # Retrieve a service by id
     def select_service(self, service_id):
         select_query = """
             SELECT services.id, type, cost, name, dog_id, completed FROM services
@@ -49,13 +55,12 @@ class DogGrommingDB:
 
         return self._cursor.fetchone()
     
+    # Create a new service
     def insert_service(self, service):
         insert_query = """
             INSERT INTO services (type, cost, dog_id, completed)
             VALUES (%s, %s, %s, %s);
         """
-
-        #print(f"Type is {service.type}, cost is {service.cost}, dog is {service.dogID}")
 
         self._cursor.execute(insert_query, (service.type, service.cost, service.dogID, service.completed))
         self._cursor.execute("SELECT LAST_INSERT_ID() service_id")
@@ -63,16 +68,18 @@ class DogGrommingDB:
         self._db_conn.commit()
         return service_id
     
+    # Update an existing service
     def update_service(self, service_id, new_type, new_cost, new_complete_status):
         update_query = """
             UPDATE services
             SET type=%s, cost=%s, completed=%s
             WHERE id=%s;
         """
-        print(f"id: {service_id}, newType: {new_type}, newCost: {new_cost}, status: {new_complete_status}")
+        
         self._cursor.execute(update_query, (new_type, new_cost, new_complete_status, service_id))
         self._db_conn.commit()
     
+    # Delete a service by id
     def delete_service_by_id(self, service_id):
         delete_query = """
             DELETE from services
@@ -82,6 +89,7 @@ class DogGrommingDB:
         self._cursor.execute(delete_query, (service_id,))
         self._db_conn.commit()
 
+    # Insert a customer to customers table
     def insert_customer(self, new_customer):
         insert_customer_query = """
             INSERT INTO customers (firstName, lastName)
@@ -94,17 +102,15 @@ class DogGrommingDB:
         self._db_conn.commit()
         return customer_id
 
-
+    # Insert a dog to dogs table
     def insert_dog(self, new_dog):
         insert_dog_query = """
             INSERT INTO dogs (name, breed, age, customer_id)
             VALUES (%s, %s, %s, %s);
         """
 
-        #print(f"Type is {service.type}, cost is {service.cost}, dog is {service.dogID}")
-
         self._cursor.execute(insert_dog_query, (new_dog.name, new_dog.breed, new_dog.age, new_dog.customerID))
         self._cursor.execute("SELECT LAST_INSERT_ID() dog_id")
-        service_id = self._cursor.fetchone()
+        dog_id = self._cursor.fetchone()
         self._db_conn.commit()
-        return service_id
+        return dog_id
--- views/services_view.py
+++ views/services_view.py
@@ -7,18 +7,25 @@ from models.dog import Dog
 
 service_list_blueprint = Blueprint('service_list_blueprint', __name__)
 
+# This route represents the home page where the custom view with service, dog, customer information is displayed
 @service_list_blueprint.route('/', methods=["GET"])
 def index():
     database = DogGrommingDB(g.mysql_db, g.mysql_cursor)
+
+    #Retrieve all the services, dogs, customer information
     all_services = database.select_all_services()
     return render_template('index.html', all_services=all_services)
 
+# This route represents a HTML form where the user can fill out the new service information
 @service_list_blueprint.route('/add_service', methods=["GET"])
 def service_entry():
    database = DogGrommingDB(g.mysql_db, g.mysql_cursor)
+
+   #Retrieve all the rows from the dogs table so that we can present the dogs list in a select box in service_entry.html
    all_dogs = database.select_all_dogs()
    return render_template("service_entry.html", all_dogs=all_dogs)
 
+# This route is a POST method and helps create a new service with the information from add service HTML page
 @service_list_blueprint.route('/add_service', methods=["POST"])
 def add_service():
    service_type = request.form.get("service_type")
@@ -32,14 +39,19 @@ def add_service():
 
    return redirect('/')
 
+# This route represents a HTML form where the user can update the existing service
 @service_list_blueprint.route('/edit_service/<int:service_id>', methods=["GET"])
 def service_edit(service_id):
    database = DogGrommingDB(g.mysql_db, g.mysql_cursor)
+
    all_dogs = database.select_all_dogs()
+
+   # Retreive a specific service so that we can display the existing service information in edit page
    service = database.select_service(service_id=service_id)
 
    return render_template("edit_service.html", all_dogs=all_dogs, service=service)
 
+# This route a POST method and helps update the existing service with the information from edit service HTML page
 @service_list_blueprint.route('/update_service', methods=["POST"])
 def update_service():
    service_id = request.form.get("service_id")
@@ -49,6 +61,7 @@ def update_service():
 
    new_complete_status_bool = False
 
+   # HTML checkbox value represents on and off values. So, we need to convert them to True and False for database operations
    if (new_complete_status == 'on'): 
       new_complete_status_bool = True
    else:
@@ -59,6 +72,7 @@ def update_service():
 
    return redirect('/')
 
+# This route deletes a selected service and redirects to the home page
 @service_list_blueprint.route('/delete_service/<int:service_id>', methods=["GET"])
 def delete_service(service_id):
    database = DogGrommingDB(g.mysql_db, g.mysql_cursor)
@@ -66,16 +80,19 @@ def delete_service(service_id):
 
    return redirect('/')
 
+# This routes displays all the completed services
 @service_list_blueprint.route('/view_completed_services', methods=["GET"])
 def view_completed_services():
     database = DogGrommingDB(g.mysql_db, g.mysql_cursor)
     all_completed_services = database.select_all_completed_services()
     return render_template('completed_services.html', all_completed_services=all_completed_services)
 
+# This route represents the HTML form where the user can fill out the dog information to create a new dog entry
 @service_list_blueprint.route('/add_dog', methods=["GET"])
 def dog_entry():
    return render_template("dog_entry.html")
 
+# This route is a POST method and creates both customer and dog entries in the db
 @service_list_blueprint.route('/add_dog', methods=["POST"])
 def add_dog():
    dog_name = request.form.get("dog_name")
@@ -87,8 +104,11 @@ def add_dog():
    new_customer = Customer(customer_first_name, customer_last_name)
 
    database = DogGrommingDB(g.mysql_db, g.mysql_cursor)
+
+   # Create a new customer entry first and get the customer id for new dog
    new_customer_obj = database.insert_customer(new_customer)
 
+   # Create a new dog entry using the customer id from above
    new_dog = Dog(dog_name, dog_breed, dog_age, new_customer_obj["customer_id"])
    database.insert_dog(new_dog)
 
